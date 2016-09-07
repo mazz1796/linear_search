dfd47de35d0658e1a51f53c09c8ae0fd243679d9
Koji Hiramatsu <mazz1796@gmail.com>
2016-09-07 15:39:37 +0800

    added global_linear

--- linear-search.rb
+++ linear-search.rb
@@ -1,4 +1,5 @@
 # Place your solutions here
+=begin
 require 'byebug'
 
 def linear_search(num, array)
@@ -21,3 +22,28 @@ end
 ramdom_numbers = [6, 29, 18, 2, 72, 19, 18, 10, 37]
 p linear_search(18, ramdom_numbers)
 p linear_search(9, ramdom_numbers)
+=end
+
+def global_linear_search(item, array)
+
+ result = []
+
+
+ 	for i in 0...array.length
+	 	if array[i] == item
+	 		result << i
+	 	end
+	 	
+	 	i += 1
+
+    end
+  result
+end
+
+
+bananas_arr = "bananas".split(//)
+new_arr = "soccer".split(//)
+
+
+p global_linear_search("a", bananas_arr)
+p global_linear_search("r", new_arr)
